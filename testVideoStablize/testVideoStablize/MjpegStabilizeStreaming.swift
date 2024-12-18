d0d1b6b021c89dbf801deaddb1d1c2c6bee889be
Thịnh Nguyễn <thinhnguyen12389@gmail.com>
2024-12-18 15:58:33 +0700

    Optimized

--- testVideoStablize/testVideoStablize/MjpegStabilizeStreaming.swift
+++ testVideoStablize/testVideoStablize/MjpegStabilizeStreaming.swift
@@ -1,10 +1,3 @@
-//
-//  MjpegStabilizeStreaming.swift
-//  testVideoStablize
-//
-//  Created by Thinh Nguyen on 18/12/24.
-//
-
 import Foundation
 import UIKit
 import AVKit
@@ -20,132 +13,130 @@ class MjpegStabilizeStreaming: NSObject, URLSessionDataDelegate {
         case playing
     }
     
-    fileprivate var receivedData: NSMutableData?
+    fileprivate var receivedData = Data()
     fileprivate var dataTask: URLSessionDataTask?
-    fileprivate var session: Foundation.URLSession!
+    fileprivate var session: URLSession!
     fileprivate var status: Status = .stopped
-    
-    open var authenticationHandler: ((URLAuthenticationChallenge) -> (Foundation.URLSession.AuthChallengeDisposition, URLCredential?))?
-    open var didStartLoading: (()->Void)?
-    open var didFinishLoading: (()->Void)?
+
+    open var authenticationHandler: ((URLAuthenticationChallenge) -> (URLSession.AuthChallengeDisposition, URLCredential?))?
+    open var didStartLoading: (() -> Void)?
+    open var didFinishLoading: (() -> Void)?
+    open var onError: ((Error?) -> Void)?
     open var contentURL: URL?
     open var imageView: UIImageView
-    
-    private var imageData = Data()
-    
+
     private let imageStabilizer = ImageStabilizer()
-    private let imageStabilizerWrapper = ImageStabilizerWrapper()
-    
     private var frameBuffer = [UIImage]() // Collect frames here
     private let frameBufferLimit = 3
-    private let queue = DispatchQueue(label: "com.mjpegstreamingcontroller.frames") // Ensure thread-safety
+    private let processingQueue = DispatchQueue(label: "com.mjpegStabilizeStreaming.frames")
     private var isProcessingFrames = false
-    
+
     public init(imageView: UIImageView) {
         self.imageView = imageView
         super.init()
-        self.session = Foundation.URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 30
+        self.session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
     }
-    
+
     public convenience init(imageView: UIImageView, contentURL: URL) {
         self.init(imageView: imageView)
         self.contentURL = contentURL
     }
-    
+
     deinit {
         dataTask?.cancel()
     }
-    
-    open func play(url: URL){
+
+    open func play(url: URL) {
         if status == .playing || status == .loading {
             stop()
         }
         contentURL = url
         play()
     }
-    
+
     open func play() {
-        guard let url = contentURL , status == .stopped else {
-            return
-        }
-        
+        guard let url = contentURL, status == .stopped else { return }
+
         status = .loading
         DispatchQueue.main.async { self.didStartLoading?() }
-        
-        receivedData = NSMutableData()
+
+        receivedData = Data()
         let request = URLRequest(url: url)
         dataTask = session.dataTask(with: request)
         dataTask?.resume()
     }
-    
-    open func stop(){
+
+    open func stop() {
         status = .stopped
         dataTask?.cancel()
     }
-    
-    // MARK: - NSURLSessionDataDelegate
-    
+
+    // MARK: - URLSessionDataDelegate
+
     open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            onError?(NSError(domain: "Invalid Response", code: -1, userInfo: nil))
+            completionHandler(.cancel)
+            return
+        }
+
+        if status == .loading {
+            status = .playing
+            DispatchQueue.main.async { self.didFinishLoading?() }
+        }
         
-        if let imageData = receivedData , imageData.length > 0,
-            let receivedImage = UIImage(data: imageData as Data) {
-            
-            // I'm creating the UIImage before performing didFinishLoading to minimize the interval
-            // between the actions done by didFinishLoading and the appearance of the first image
-            if status == .loading {
-                status = .playing
-                DispatchQueue.main.async { self.didFinishLoading?() }
-            }
-            
-            queue.async { [weak self] in
-                guard let self else { return }
-                self.frameBuffer.append(receivedImage)
-                
-                if self.frameBuffer.count == self.frameBufferLimit {
+        if let image = UIImage(data: receivedData) {
+            processingQueue.async { [weak self] in
+                guard let self = self else { return }
+                self.frameBuffer.append(image)
+
+                if self.frameBuffer.count >= self.frameBufferLimit {
                     guard !self.isProcessingFrames else { return }
                     self.isProcessingFrames = true
-                    
+
                     DispatchQueue.global(qos: .userInitiated).async {
                         let stabilizedImages = self.imageStabilizer.stabilized(withImageList: self.frameBuffer)
                         self.frameBuffer.removeAll()
-                        
-                        // Display stabilized images on the main thread
+
                         DispatchQueue.main.async {
                             self.isProcessingFrames = false
-                            
-                            stabilizedImages?.forEach({
-                                self.imageView.image = $0 as? UIImage
-                            })
+                            stabilizedImages?.forEach { stabilizedImage in
+                                self.imageView.image = stabilizedImage as? UIImage
+                            }
                         }
                     }
                 }
             }
         }
-        
-        receivedData = NSMutableData()
+        receivedData = Data()
         completionHandler(.allow)
     }
-    
+
     open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        receivedData?.append(data)
+        receivedData.append(data)
     }
-    
-    // MARK: - NSURLSessionTaskDelegate
-    
+
     open func urlSession(_ session: URLSession, task: URLSessionTask, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        
         var credential: URLCredential?
-        var disposition: Foundation.URLSession.AuthChallengeDisposition = .performDefaultHandling
-        
-        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
-            if let trust = challenge.protectionSpace.serverTrust {
-                credential = URLCredential(trust: trust)
-                disposition = .useCredential
-            }
+        var disposition: URLSession.AuthChallengeDisposition = .performDefaultHandling
+
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
+           let trust = challenge.protectionSpace.serverTrust {
+            credential = URLCredential(trust: trust)
+            disposition = .useCredential
         } else if let onAuthentication = authenticationHandler {
             (disposition, credential) = onAuthentication(challenge)
         }
-        
+
         completionHandler(disposition, credential)
+    }
+
+    // MARK: - Error Handling
+    private func handleError(_ error: Error?) {
+        DispatchQueue.main.async {
+            self.onError?(error)
+        }
     }
 }
