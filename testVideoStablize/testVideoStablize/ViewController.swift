20ae076d3b95d73e1d2f2e841c155f2e1e54aee6
PhuongPhamHoang <phuong.phamhoangitt@gmail.com>
2025-03-13 11:48:44 +0700

    Update ViewController.swift

--- testVideoStablize/testVideoStablize/ViewController.swift
+++ testVideoStablize/testVideoStablize/ViewController.swift
@@ -2,15 +2,19 @@ import UIKit
 import AVKit
 import Vision
 import CoreImage
+import MobileVLCKit
 
 class ViewController: UIViewController {
     private let streamURL = URL(string: "http://192.168.1.18:81/trek_stream")!
+    private let vlcStreamURL = URL(string: "http://192.168.1.18:81/stream")!
+    private let gyroscopeURL = URL(string: "http://192.168.1.18/gyroscope")!
     
     // Define two UIImageViews
     var imageView1: UIImageView = .init(frame: .zero)
     var imageView2: UIImageView = .init(frame: .zero)
     var mjpegStreamView2: MjpegStabilizeStreaming!
     var mjpegStreamView1: MjpegStreaming!
+    var gyroscopeExtractor: GyroscopeExtractor!
     
     // Labels to display rotation values
     var firstRotationLabel: UILabel = .init(frame: .zero)
@@ -18,24 +22,21 @@ class ViewController: UIViewController {
     
     let startRotateButton: UIButton = .init(frame: .zero)
     
+    // VLC
+    private let mediaPlayer = VLCMediaPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         // setupImageView(imageView: imageView1)
         setupImageView(imageView: imageView2)
+        setupVLCPlayer()
+        setupGyroscopeExtractor()
+//        setupMjpegStabilizeStreaming()
         
 //        let mjpegStreamView1 = MjpegStreaming(imageView: imageView1)
 //        mjpegStreamView1.contentURL = streamURL
 //        mjpegStreamView1.play()
-        
-        mjpegStreamView2 = MjpegStabilizeStreaming(imageView: imageView2)
-        mjpegStreamView2.contentURL = streamURL
-        mjpegStreamView2.rotationUpdateHandler = { [weak self] firstRotation, currentRotation in
-            DispatchQueue.main.async {
-                self?.updateRotationLabels(firstRotation: firstRotation, currentRotation: currentRotation)
-            }
-        }
-        mjpegStreamView2.play()
         
         let rotationSwitch = UISwitch()
         rotationSwitch.isHidden = true
@@ -95,7 +96,7 @@ class ViewController: UIViewController {
             imageView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             imageView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
             imageView2.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            imageView2.heightAnchor.constraint(equalToConstant: 100),
+            imageView2.heightAnchor.constraint(equalToConstant: 300),
             
             rotationLabel.topAnchor.constraint(equalTo: imageView2.bottomAnchor, constant: 150),
             rotationLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
@@ -122,8 +123,44 @@ class ViewController: UIViewController {
         ])
     }
     
+    private func setupVLCPlayer() {
+        
+        mediaPlayer.drawable = imageView2
+        mediaPlayer.media = VLCMedia(url: vlcStreamURL)
+        mediaPlayer.media.addOptions([
+            "network-caching": "1000", // Buffering value in ms
+            "live-caching": "1000"     // Live stream buffering
+        ])
+        mediaPlayer.play()
+    }
+    
+    private func setupMjpegStabilizeStreaming() {
+        mjpegStreamView2 = MjpegStabilizeStreaming(imageView: imageView2)
+        mjpegStreamView2.contentURL = streamURL
+        mjpegStreamView2.rotationUpdateHandler = { [weak self] firstRotation, currentRotation in
+            DispatchQueue.main.async {
+                self?.updateRotationLabels(firstRotation: firstRotation, currentRotation: currentRotation)
+            }
+        }
+        mjpegStreamView2.play()
+
+    }
+    
+    private func setupGyroscopeExtractor() {
+        gyroscopeExtractor = GyroscopeExtractor(imageView: imageView2, containerView: view)
+        gyroscopeExtractor.contentURL = gyroscopeURL
+        gyroscopeExtractor.rotationUpdateHandler = { [weak self] firstRotation, currentRotation in
+            DispatchQueue.main.async {
+                self?.updateRotationLabels(firstRotation: firstRotation, currentRotation: currentRotation)
+            }
+        }
+        gyroscopeExtractor.playStream()
+        gyroscopeExtractor.startGyroscopeUpdates()
+    }
+    
     @objc func toggleRotation(_ sender: UISwitch) {
-        mjpegStreamView2.enableRotation = sender.isOn
+        // mjpegStreamView2.enableRotation = sender.isOn
+        gyroscopeExtractor.enableRotation = sender.isOn
     }
     
     @objc func toggleStabilization(_ sender: UISwitch) {
@@ -131,12 +168,19 @@ class ViewController: UIViewController {
     }
     
     @objc func onAutoRotation(_ sender: UIButton) {
-        if mjpegStreamView2.startAutoRotation {
+//        if mjpegStreamView2.startAutoRotation {
+//            startRotateButton.setTitle("Start Auto Rotation & Auto Stabilization", for: .normal)
+//            mjpegStreamView2.startAutoRotation = false
+//        } else {
+//            startRotateButton.setTitle("Stop Auto Rotation & Auto Stabilization", for: .normal)
+//            mjpegStreamView2.startAutoRotation = true
+//        }
+        if gyroscopeExtractor.startAutoRotation {
             startRotateButton.setTitle("Start Auto Rotation & Auto Stabilization", for: .normal)
-            mjpegStreamView2.startAutoRotation = false
+            gyroscopeExtractor.startAutoRotation = false
         } else {
             startRotateButton.setTitle("Stop Auto Rotation & Auto Stabilization", for: .normal)
-            mjpegStreamView2.startAutoRotation = true
+            gyroscopeExtractor.startAutoRotation = true
         }
     }
     
@@ -146,6 +190,7 @@ class ViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
         imageView.backgroundColor = .white
+        imageView.clipsToBounds = true
     }
     private func updateRotationLabels(firstRotation: String?, currentRotation: String?) {
         // firstRotationLabel.text = "First: \(firstRotation ?? "")"
